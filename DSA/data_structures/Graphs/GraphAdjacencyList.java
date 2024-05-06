a03bdff9599fe460e55a973373c2101295669ef2
Arunmozhi C <arun.nambi21@gmail.com>
2024-05-07 01:04:32 +0530

    refactors

---
+++ DSA/data_structures/Graphs/GraphAdjacencyList.java
@@ -0,0 +1,15 @@
+package data_structures.Graphs;
+
+import java.util.Scanner;
+
+
+class GraphAdjacencyList{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        String s = new String();
+
+//        HashMap<>
+    }
+}
---
+++ DSA/data_structures/Graphs/GraphMatrixRepresentation.java
@@ -0,0 +1,7 @@
+package data_structures.Graphs;
+
+public class GraphMatrixRepresentation{
+    public static void main(String[] args) {
+        System.out.print("Hello!");
+    }
+}
